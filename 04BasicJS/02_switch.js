5e50892d51410801f9b341883bde9cbe5a3639ea
prosanto20 <136997485+prosanto20@users.noreply.github.com>
2026-04-24 15:07:24 +0000

    switch case

---
+++ 04BasicJS/02_switch.js
@@ -0,0 +1,32 @@
+// node 04BasicJS/02_switch.js
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("august");
+        break;
+    default:
+        console.log("default case match");
+        break;
+}
