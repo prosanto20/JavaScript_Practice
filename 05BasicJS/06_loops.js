eb0e4199eb2c450cf15cea1de84fe1e29fcb9cd1
prosanto20 <136997485+prosanto20@users.noreply.github.com>
2026-04-27 18:33:20 +0000

    forEach and filter basic

---
+++ 05BasicJS/06_loops.js
@@ -0,0 +1,46 @@
+// node 05BasicJS/06_loops.js
+
+const codding = ["js","rb","py","java","cpp"]
+
+const values = codding.forEach( (item) =>{
+    //console.log(item);
+    return item
+})
+//console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9]
+// const newNums=myNums.filter( (num) => num >4)
+const newNums=myNums.filter( (num) =>{ 
+    return num >4
+})
+// console.log(newNums);
+
+const newNums1 =[]
+myNums.forEach( (num) =>{
+    if(num>4)
+    {newNums1.push(num)}
+})
+//console.log(newNums1);
+
+// example
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+    let user = books.filter( (bk) => bk.genre === "Science")
+
+    user = books.filter( (bk) => { 
+    return bk.publish >= 2010 && bk.genre === "Science"
+    })
+
+
+  console.log(user);
+  
