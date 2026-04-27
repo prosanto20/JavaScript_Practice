a440bc08111cde612a8eb6b03aee892fcfe2a5fb
prosanto20 <136997485+prosanto20@users.noreply.github.com>
2026-04-27 18:47:03 +0000

    chaining

---
+++ 05BasicJS/07_loops.js
@@ -0,0 +1,17 @@
+// node 05BasicJS/07_loops.js
+
+const myNums = [1,2,3,4,5,6,7,8,9,20]
+
+const newNums = myNums.map( (num) => num +10)
+const newNums1 = myNums.map( (num) => {
+    return num +10})
+
+//console.log(newNums);
+
+const newNums2 = myNums
+                .map( (num) => num * 10)
+                .map( (num) => num +1 )
+                .filter( (num) => num >40)
+
+console.log(newNums2);
+
